2eca641d2487d5a2b405cc453eeca1445a80dfd1
VisheshCoderBoy <86160168+VisheshCoderBoy@users.noreply.github.com>
2021-06-19 20:39:47 +0530

    Starting with Python #1
    This was the basic program a coder needs to know before about Python

---
+++ 1.py
@@ -0,0 +1,14 @@
+#operators
+#arithmetic +,-,/,*,//,%,**
+#comparison ==,!=,<=,>=,>,<
+#bitwise and, or, not
+a = 12
+b = 13
+print(a+b)
+print(a-b)
+print(a/b)
+print(a*b)
+print(a//b)
+print(a%b)
+print(a**b)
+ 
